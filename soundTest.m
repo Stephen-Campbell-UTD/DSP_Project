8961d8c91c7dd2b25160c5e14c7599d91e1f13a4
Stephen <sac170630@utdallas.edy>
2021-11-24 16:28:52 -0600

    Proper Separation

---
+++ soundTest.m
@@ -0,0 +1,9 @@
+prompt = "Press Enter"
+input(prompt);
+sound(5*dec_left,Fs/2);
+input(prompt);
+sound(5*dec_right,Fs/2);
+input(prompt);
+sound(5*dec_mono,Fs/25);
+input(prompt);
+sound(5*dec_sub,Fs/25);
